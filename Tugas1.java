ec02ee9a72123aa690b2b50a77bbddcb4ccfef40
kianallent <111202315496@mhs.dinus.ac.id>
2025-03-05 13:58:27 +0700

    first commit

---
+++ Tugas1.java
@@ -0,0 +1,31 @@
+package tugasweek1;
+
+class Tugas1{
+    public static void main(String[] biodata){
+        String nim;
+        nim = "A11.2023.15402";
+        String nama;
+        nama = "Kian Allentabhima Rizqiawan";
+        String alamat;
+        alamat = "Lemahabang, tanjung, Brebes";
+        String kota;
+        kota = "Brebes";
+        String kodepos;
+        kodepos = "52254";
+        String telp;
+        telp = "083861515955";
+        String hp;
+        hp = "083861515955";
+        String email;
+        email = "111202315402@mhs.dinus.ac.id";
+
+        System.out.println("Nim :"+nim);
+        System.out.println("Nama :"+nama);
+        System.out.println("alamat :"+alamat);
+        System.out.println("kota :"+kota);
+        System.out.println("kodepos :"+kodepos);
+        System.out.println("telp :"+telp);
+        System.out.println("hp :"+hp);
+        System.out.println("email :"+email);
+    }
+}
